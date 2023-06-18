5be425492f78f687e996b78abfce87609fb5dc9b
NehaL-8 <116095754+NehaL-8@users.noreply.github.com>
2023-06-18 12:28:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,15 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph:boolean,
 }
 
 /**
  * The parent element of the react app.
  * It renders title, button and Graph react element.
- */
+ */interface PerspectiveViewerElement extends HTMLElements {
+  load: (table :Table)=> void,
+}
 class App extends Component<{}, IState> {
   constructor(props: {}) {
     super(props);
@@ -22,6 +25,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph:false,
     };
   }
 
@@ -29,6 +33,7 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if(this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
   }
 
@@ -36,11 +41,21 @@ class App extends Component<{}, IState> {
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
+    let x=0
+    constant interval = set Interval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
+      data:serverResponds,
+        showGraph:true,
     });
+  });
+    x++;
+    if(x>1000) {
+      clearInertval(interval);
+    }
+  },100
   }
 
   /**
